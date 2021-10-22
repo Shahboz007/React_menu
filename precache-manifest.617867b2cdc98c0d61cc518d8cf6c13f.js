a31fbc0aee6bafd79fd09f44b77e793d25f82c9d
Shahboz007 <olmasovshahboz007@gmail.com>
2021-10-22 19:09:36 +0500

    Updates

---
+++ precache-manifest.617867b2cdc98c0d61cc518d8cf6c13f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "758fa9053e69c4caaedf589868f53cf9",
+    "url": "/menu/index.html"
+  },
+  {
+    "revision": "fde99d24b0b185e131a9",
+    "url": "/menu/static/css/main.99af19dc.chunk.css"
+  },
+  {
+    "revision": "d6da81e89034037073de",
+    "url": "/menu/static/js/2.d62a9384.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/menu/static/js/2.d62a9384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fde99d24b0b185e131a9",
+    "url": "/menu/static/js/main.4e845dd8.chunk.js"
+  },
+  {
+    "revision": "2acabc264f1fa501847b",
+    "url": "/menu/static/js/runtime-main.bab3c470.js"
+  }
+]);
